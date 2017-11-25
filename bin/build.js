d33436447a650a223a97b11ecdeb93485aec4fff
Jonathan Dupré <hello@jonathandupre.com>
2017-11-25 00:39:31 -0500

    add build css to build script

--- bin/build.js
+++ bin/build.js
@@ -1,9 +1,12 @@
 var mkdirp = require('mkdirp')
 var buildHTML = require('./build-html')
+var buildCSS = require('./build-css')
 
 mkdirp.sync('dist')
 buildHTML(function (err) {
+  buildCSS(function (err) {
   
+  })
 })
 
 
